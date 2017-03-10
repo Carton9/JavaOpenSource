2261f1eb510c4355d81d724501d859ab40d0eb44
Carton9 <qazwqsx9@gmail.com>
2017-03-09 21:49:30 -0800

    Revert "Revert "testing web server""
    This reverts commit 59b816e8bf888cf8d2f0c38c8216000e6e63125d.

--- Adv/src/adv/Web/MainTest.java
+++ Adv/src/adv/Web/MainTest.java
@@ -6,21 +6,19 @@ public class MainTest {
 	static byte[] bb=new byte[4096];
 	public static void main(String[] args) throws UndefineProtocolException, IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		WebClient testClient=new SimpleClient(Protocol.TCP,"127.0.0.1",12345);
-		Thread a=new Thread(testClient);
-		a.start();
+		WebServer testClient=new SimpleServer("A",Protocol.TCP,"127.0.0.1",12345);
+		//Thread a=new Thread(testClient);
+		testClient.run();
 		while(true){
 			//a.suspend();
-			byte[] buff=testClient.revice();
-			if(buff==null){
-				continue;
-			}
-			System.out.println(new String(buff));
-			
+			//byte[] buff=testClient.revice();
+			//if(buff==null){
+			//	continue;
+			///}
+			//System.out.println(new String(buff));
+			//testClient.send("best client".getBytes());
 			//a.resume();
 		}
 	}
-	static void print(){
-		System.out.println(new String(bb));
-	}
+
 }
